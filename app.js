b478ca025664523971696342620d76f652f24512
larissa-garc <larissaicrag@gmail.com>
2025-02-04 23:31:30 -0300

    Trabalho finalizado

--- app.js
+++ app.js
@@ -5,16 +5,46 @@ let amigos = [];
 
 function adicionarAmigo(){
     let novoAmigo = document.querySelector('input');
-        if (novoAmigo == ''){
+        if (novoAmigo.value === ''){
             alert ('Por favor, insira um nome');
         } else {
-            amigos.push(novoAmigo);
+            amigos.push(novoAmigo.value);
             limparCampo();
-
+            console.log(amigos);
+            atualizarListaAmigos();
+            
         }
 }
 
 function limparCampo(){
-    novoAmigo = document.querySelector('input');
+    let novoAmigo = document.querySelector('input');
     novoAmigo.value = '';
 }
+
+function atualizarListaAmigos(){
+    let lista = document.getElementById('listaAmigos');
+    lista.innerHTML = '';
+    
+    amigos.forEach(amigo =>{
+    let li = document.createElement('li');
+    li.textContent = amigo;
+    lista.appendChild(li);
+    })
+
+}
+
+function sortearAmigo(){
+    if (amigos.length === 0){
+        alert('Lista de amigos está vazia.');
+    }
+    else {
+        let sorteio = Math.floor(Math.random()*amigos.length);
+        let amigoSorteado = amigos[sorteio];
+        console.log(sorteio);
+
+        let resultado = document.getElementById('resultado');
+        listaAmigos.innerHTML = '';
+        resultado.innerHTML = "O amigo secreto sorteado é: "+amigoSorteado;
+    }
+}
+
